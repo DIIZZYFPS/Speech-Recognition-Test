2ebf833b5d5b96d00440cf6c345bf7232f16dfda
Damarrion Morgan Harper <dgodsonmo@gmail.com>
2025-02-25 23:30:37 -0800

    stuff

--- src/concurrentSpeech.py
+++ src/concurrentSpeech.py
@@ -7,7 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "openai/whisper-large-v3-turbo"
+model_id = "openai/whisper-tiny.en"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=False, use_safetensors=True
